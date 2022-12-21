08a510988fa7106dbba9166f86f3214dda068a0e
Stewie <nhut170501@gmail.com>
2022-12-21 08:53:57 +0700

    fix order

--- app/src/main/java/com/example/flashfastfood/Fragment/OrderFragment.java
+++ app/src/main/java/com/example/flashfastfood/Fragment/OrderFragment.java
@@ -43,19 +43,6 @@ import java.util.Calendar;
 
 public class OrderFragment extends Fragment {
 
-    private String currentUserId;
-
-    private RecyclerView rvDelivery;
-
-    private FirebaseDatabase firebaseDatabase;
-    private DatabaseReference orderRef;
-
-    private FirebaseRecyclerOptions<Order> options;
-    private FirebaseRecyclerAdapter<Order, OrderViewHolder> adapter;
-    private ArrayList<String> arrayList2 = null;
-
-    private LottieAnimationView deliveryWaiting;
-
     TabLayout tabLayout;
     ViewPager viewPager;
 
@@ -88,106 +75,6 @@ public class OrderFragment extends Fragment {
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
 
-        firebaseDatabase = FirebaseDatabase.getInstance("https://flashfastfood-81fee-default-rtdb.asia-southeast1.firebasedatabase.app");
-        orderRef = firebaseDatabase.getReference("Order");
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        currentUserId = user.getUid();
-
-        rvDelivery = view.findViewById(R.id.rvDelivery);
-        rvDelivery.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-
-        deliveryWaiting = view.findViewById(R.id.deliveryWaiting);
-        deliveryWaiting.setVisibility(View.GONE);
     }
 
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        arrayList2 = new ArrayList<>();
-        loadDeliveringItems();
-        String countItemInCart = Integer.toString(arrayList2.size());
-        int itemInOrderDe = Integer.parseInt(countItemInCart);
-        if (itemInOrderDe == 0) {
-            deliveryWaiting.setVisibility(View.VISIBLE);
-        } else {
-            deliveryWaiting.setVisibility(View.GONE);
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        arrayList2 = new ArrayList<>();
-        loadDeliveringItems();
-//        String countItemInCart= Integer.toString(arrayList2.size());
-//        int itemInOrder = Integer.parseInt(countItemInCart);
-//        if (itemInOrder==0){
-//            deliveryWaiting.setVisibility(View.VISIBLE);
-//        }
-//        else {
-//            deliveryWaiting.setVisibility(View.GONE);
-//        }
-    }
-
-    private void loadDeliveringItems() {
-        Query query = orderRef.child(currentUserId).orderByChild("orderStatus").equalTo("Delivering");
-
-        options = new FirebaseRecyclerOptions.Builder<Order>().setQuery(query, Order.class).build();
-
-        adapter = new FirebaseRecyclerAdapter<Order, OrderViewHolder>(options) {
-            @Override
-            protected void onBindViewHolder(@NonNull OrderViewHolder holder, int position, @NonNull Order model) {
-                String postKey = getRef(position).getKey();
-
-                orderRef.child(currentUserId).child(postKey).addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                            arrayList2.add(dataSnapshot.getKey());
-                        }
-
-                        String orderDate = snapshot.child("orderDate").getValue().toString();
-                        String orderTime = snapshot.child("orderTime").getValue().toString();
-                        String orderPrice = snapshot.child("orderTotalPrice").getValue().toString();
-                        String orderPayment = snapshot.child("orderPayment").getValue().toString();
-                        String orderItemQuantity = snapshot.child("orderItemQuantity").getValue().toString();
-                        String orderLocation = snapshot.child("orderLocation").getValue().toString();
-                        String orderStatus = snapshot.child("orderStatus").getValue().toString();
-
-                        holder.orderItemQuantity.setText(orderItemQuantity);
-                        holder.orderPrice.setText(orderPrice);
-                        holder.orderLocation.setText(orderLocation);
-                        holder.orderStatus.setText(orderStatus);
-                        holder.orderDate.setText(orderDate);
-                        holder.orderTime.setText(orderTime);
-                        holder.orderPayment.setText(orderPayment);
-
-                        if (orderPayment.equals("Cash on delivery")) {
-                            holder.orderPayment.setTextColor(Color.parseColor("#CB1B11"));
-                        } else {
-                            holder.orderPayment.setTextColor(Color.parseColor("#4CAF50"));
-                        }
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                    }
-                });
-            }
-
-            @NonNull
-            @Override
-            public OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_order_delivery, parent, false);
-                OrderViewHolder viewHolder = new OrderViewHolder(view);
-                return viewHolder;
-            }
-        };
-
-        rvDelivery.setAdapter(adapter);
-        adapter.startListening();
-    }
 }
